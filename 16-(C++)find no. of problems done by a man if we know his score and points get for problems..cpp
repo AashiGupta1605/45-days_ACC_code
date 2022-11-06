8bbcd2fec61540d3fe8d056811b694e8ebc2f4ff
AashiGupta1605 <115386205+AashiGupta1605@users.noreply.github.com>
2022-11-06 23:50:35 +0530

    Add files via upload

---
+++ 16-(C++)find no. of problems done by a man if we know his score and points get for problems..cpp
@@ -0,0 +1,34 @@
+/*Problem
+There are 10 problems in a contest. You know that the score of each problem is either 1 or 100 points.
+
+Chef came to know the total score of a participant and he is wondering how many problems were actually solved by that participant.
+
+Given the total score P of the participant, determine the number of problems solved by the participant. Print -1 in case the score is invalid.
+
+Input Format
+First line will contain T, number of test cases. Then the test cases follow.
+Each test case contains of a single line containing a single integer P - denoting the number of points scored by the participant.
+
+Output Format
+For each testcase, output the number of problems solved by the participant or -1 if the score is invalid.*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,p;
+	cin>>t;
+	while(t!=0){
+	    cin>>p;
+	    int c=0;
+	    c=(p/100)+(p%100);
+	    if(c<=10)
+	    cout<<c<<endl;
+	    else
+	    cout<<"-1\n";
+	    t--;
+	}
+	return 0;
+}
+
